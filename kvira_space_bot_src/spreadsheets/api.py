085689aaf7d763a39953457f32a6919efa32f00a
Mikhail Solovyanov <mikhail.solovyanov@gmail.com>
2024-07-05 16:36:50 +0400

    Initial commit

---
+++ kvira_space_bot_src/spreadsheets/api.py
@@ -0,0 +1,12 @@
+# File to define the API for the application
+
+
+def check_if_user_exists(username: str) -> bool:
+    pass
+
+def punch_user_day(username: str):
+    pass
+
+def get_days_left(username: str):
+    pass
+
