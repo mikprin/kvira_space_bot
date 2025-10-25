d1a7d9f6735f711b21c74f1520302d0a87e0b3d1
brontozyablik <anya.yaveyn@gmail.com>
2025-10-25 21:59:31 +0400

    paticipants handling

--- kvira_space_bot_src/spreadsheets/halloween.py
+++ kvira_space_bot_src/spreadsheets/halloween.py
@@ -1,6 +1,8 @@
+import itertools
 import logging
+from dataclasses import dataclass
 
-from kvira_space_bot_src.spreadsheets.data import Lang, get_sheet
+from kvira_space_bot_src.spreadsheets.data import get_sheet, split_by_coma
 
 TEXTS_SHEET = 'HalloweenQuest-bot'
 
@@ -9,3 +11,47 @@ CRYPTIDS = {
     "paklya": "Paklya",
 }
 
+
+@dataclass
+class Participant:
+    row: int
+    name: str
+    clues: [str]
+
+    def cryptids(self) -> [str]:
+        return [CRYPTIDS[clue] for clue in self.clues]
+
+
+def next_empty_row():
+    sheet = get_sheet(TEXTS_SHEET)
+    for row in itertools.count(1):
+        if not sheet.cell(row, 1).value.strip:
+            return row
+
+
+def get_user(row: int) -> Participant:
+    sheet = get_sheet(TEXTS_SHEET)
+    name = sheet.cell(row, 1).strip()
+    if not name:
+        # error
+        pass
+    clues = split_by_coma(sheet.cell(row, 2))
+    for clue in clues:
+        if clue not in CRYPTIDS.keys():
+            # error
+            pass
+    return Participant(row, name, clues)
+
+
+def add_user(user: str):
+    try:
+        return add_user_(user)
+    except Exception as e:
+        logging.error(f"Error while punching the user with row id {user}: {e}")
+        return False
+
+
+def add_user_(user: str):
+    sheet = get_sheet(TEXTS_SHEET)
+    row = next_empty_row()
+    sheet.update_cell(row, 1, user)
